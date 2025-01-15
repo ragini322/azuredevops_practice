0d9c038a4094711eb1d2bd0046d04def1da9a632
ragini <upadhyayragini322@gmail.com>
2025-01-15 22:10:03 +0530

    added

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "strg"                  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storagkhan"            # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "don3"                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.111.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-tf-jhansaram-1"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "sa" {
+  depends_on               = [azurerm_resource_group.rg]
+  name                     = "saaasaram1"
+  resource_group_name      = "rg-tf-jhansaram-1"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
